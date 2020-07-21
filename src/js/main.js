6b7d2841d17a492a431a8c671d9a2059ffbdb1f0
llflannery <lflannery@seattletimes.com>
2020-07-21 15:39:03 -0700

    first

---
+++ src/js/main.js
@@ -0,0 +1,191 @@
+//load our custom elements
+require("component-leaflet-map");
+require("component-responsive-frame");
+
+//get access to Leaflet and the map
+var element = document.querySelector("leaflet-map");
+var L = element.leaflet;
+var map = element.map;
+
+
+var ich = require("icanhaz");
+var templateFile = require("./_popup.html");
+ich.addTemplate("popup", templateFile);
+
+var data = require("./take6.geo.json");
+
+// var data = require("./map-fyi.geo.json");
+
+// console.log(data);
+
+var mapElement = document.querySelector("leaflet-map");
+
+
+
+  var L = mapElement.leaflet;
+  var map = mapElement.map;
+
+  map.scrollWheelZoom.disable();
+
+  map.setView(new L.LatLng(47.4800, -121.8), 9);
+
+  var focused = false;
+
+  var deaths = "covid_Deaths";
+  var cases = "covid_Positive_Tests";
+  var tests = "covid_Test_Rate";
+  var tests_pos = "covid_Positive_Test_Rate";
+
+  var covid_Deaths_array = [10,20,30,40];
+  var covid_Positive_Tests_array  = [100,200,300,400];
+  var covid_Test_Rate_array  = [8000, 10000, 12000, 14000];
+  var covid_Positive_Test_Rate_array  = [300,600,900,1200];
+
+  var arrayLegend = {
+    covid_Deaths_array: covid_Deaths_array,
+    covid_Positive_Tests_array: covid_Positive_Tests_array,
+    covid_Test_Rate_array: covid_Test_Rate_array,
+    covid_Positive_Test_Rate_array: covid_Positive_Test_Rate_array,
+    deaths: deaths,
+    cases: cases,
+    tests: tests,
+    tests_pos: tests_pos
+  };
+
+  var commafy = s => (s * 1).toLocaleString().replace(/\.0+$/, "");
+
+  // data.features.forEach(function(f) {
+  //   ["percent", "single", "joint"].forEach(function(prop) {
+  //     f.properties[prop] = (f.properties[prop] * 100).toFixed(1);
+  //   });
+  //   ["returns"].forEach(function(prop) {
+  //     f.properties[prop] = commafy ((f.properties[prop]));
+  //   });
+  // });
+
+
+  var onEachFeature = function(feature, layer) {
+    layer.bindPopup(ich.popup(feature.properties))
+    layer.on({
+      mouseover: function(e) {
+        layer.setStyle({ weight: 2, fillOpacity: .8 });
+      },
+      mouseout: function(e) {
+        if (focused && focused == layer) { return }
+        layer.setStyle({ weight: 1, fillOpacity: 1 });
+      }
+    });
+  };
+
+  var getColor = function(d, array) {
+    var value = d;
+    var thisArray = arrayLegend[array];
+
+    // console.log(thisArray[0]);
+
+    if (typeof value == "string") {
+      value = Number(value.replace(/,/, ""));
+    }
+    // console.log(value)
+    if (typeof value != "undefined") {
+      // condition ? if-true : if-false;
+     return value >= thisArray[3] ? '#B22118' :
+     		    value >= thisArray[2] ? '#E34D32' :
+            value >= thisArray[1] ? '#ED8A52' :
+            value >= thisArray[0]  ? '#F4BD6E' :
+
+             '#FAE2C1' ;
+    } else {
+      return "gray"
+    }
+  };
+
+
+  var geojson = L.geoJson(data, {
+    onEachFeature: onEachFeature
+  }).addTo(map);
+
+
+
+  function restyleLayer(propertyName) {
+
+    geojson.eachLayer(function(featureInstanceLayer) {
+        var propertyValue = featureInstanceLayer.feature.properties[propertyName];
+
+        var colorArray = propertyName + "_array";
+
+        // console.log(colorArray);
+
+        // Your function that determines a fill color for a particular
+        // property name and value.
+        var myFillColor = getColor(propertyValue, colorArray);
+
+        featureInstanceLayer.setStyle({
+            fillColor: myFillColor,
+            opacity: .25,
+            color: '#000',
+            fillOpacity: 1,
+            weight: 1
+        });
+    });
+}
+
+restyleLayer(deaths);
+
+const startSel = document.getElementById("deaths");
+startSel.classList.add("active");
+
+
+
+var onEachFeature = function(feature, layer) {
+  layer.bindPopup(ich.popup(feature.properties))
+};
+
+ map.scrollWheelZoom.disable();
+
+ var filterMarkers = function(clickedID) {
+   hideSpans();
+   var chosenSpans = legendContainer.getElementsByClassName(clickedID);
+   showSpans(chosenSpans);
+
+   for (var i = 0; i < filterButtons.length; i++) {
+     filterButtons[i].classList.remove("active");
+   }
+   var selectedID = document.getElementById(clickedID);
+   selectedID.classList.add("active");
+   var myID = arrayLegend[clickedID];
+
+   restyleLayer(myID);
+ }
+
+
+ var filterButtons = document.getElementsByClassName("button");
+ var legendContainer = document.getElementById("legendCon");
+ var allSpans = legendContainer.getElementsByTagName('span');
+
+ var hideSpans = function() {
+   for (var i = 0; i < allSpans.length; i++) {
+     allSpans[i].classList.add("hide");
+   }
+ };
+
+ var showSpans = function(spanClass) {
+   for (var i = 0; i < spanClass.length; i++) {
+     spanClass[i].classList.remove("hide");
+   }
+ };
+
+
+hideSpans();
+var deathSpans = legendContainer.getElementsByClassName('deaths');
+
+showSpans(deathSpans);
+
+
+ for (var i = 0; i < filterButtons.length; i++) {
+    let thisID = filterButtons[i].getAttribute("id");
+
+    // filterButtons[i].style.backgroundColor = "#f8f8f8";
+
+    filterButtons[i].addEventListener('click', () => filterMarkers(thisID), false);
+}
